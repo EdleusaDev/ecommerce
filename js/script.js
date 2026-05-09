3a2805790b9fa0d3d4807ec0be6af0c81017252c
Edleusa Santos do Amaral <82529262+EdleusaDev@users.noreply.github.com>
2026-05-09 16:25:27 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -13,9 +13,11 @@ document.addEventListener("DOMContentLoaded", function () {
 
 const products = [
     { id: 1, nome: 'Sandália Feminina', preco: 79.99, image: 'img/sandalia.jpg' },
-    { id: 2, nome: 'Scarpin Preto', preco: 129.99, image: 'img/scarpin.jpg' },
+    { id: 2, nome: 'Sandalia Sandra', preco: 129.99, image: 'img/sandaliasandra.jpg' },
     { id: 3, nome: 'Tamanco Feminino', preco: 89.99, image: 'img/tamanco.jpg' },
-    { id: 4, nome: 'Bota Feminina', preco: 199.99, image: 'img/bota.jpg' }
+    { id: 4, nome: 'Bota Marrom', preco: 199.99, image: 'img/bota.jpg' },
+    { id: 5, nome: 'Bota Marsala', preco: 199.99, image: 'img/bota_marsala.jpg' },
+    { id: 5, nome: 'Bota Marsala', preco: 199.99, image: 'img/bota_marsala.jpg' }
 ];
 
 function displayProducts() {
